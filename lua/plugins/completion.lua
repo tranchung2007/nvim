9c418cbfa7b0beb162c7860159ddd6f6101daec6
tranchung2007 <banalover2ten@gmail.com>
2024-12-23 20:04:18 +0700

    update

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -14,11 +14,7 @@ return {
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -30,14 +26,11 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					--{ name = "vsnip" }, -- For vsnip users.
-					{ name = "luasnip" }, -- For luasnip users.
-					-- { name = "ultisnips" }, -- For ultisnips users.
-					-- { name = 'snippy' }, -- For snippy users.
+					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,6 +1,13 @@
 return {
 	"numToStr/FTerm.nvim",
 	config = function()
+		require("FTerm").setup({
+			border = "single",
+			dimensions = {
+				height = 0.75,
+				width = 0.7,
+			},
+		})
 		vim.keymap.set("n", "<A-t>", '<CMD>lua require("FTerm").toggle()<CR>')
 		vim.keymap.set("t", "<A-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 		vim.api.nvim_create_user_command("CppBuild", function()
